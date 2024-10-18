b6a7a057d7234c849ee59834f7659b0eb423eb07
SilasSilvaa <silas-oli@hotmail.com>
2024-10-17 22:39:52 -0300

    feat: model catalog

---
+++ CatalogAPI/Models/Catalog.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogAPI.Models;
+
+public class Catalog
+{
+    public Catalog()
+    {
+        Products = new List<Product>();
+    }
+    public int CatalogId { get; set; }
+    public string? Name { get; set; }
+    public string? Image { get; set; }
+    public ICollection<Product> Products { get; set; }
+}
