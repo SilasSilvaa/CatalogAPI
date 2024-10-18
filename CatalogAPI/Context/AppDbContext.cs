faede5d3eed85f2d66a8f3913ec8350ab8985026
SilasSilvaa <silas-oli@hotmail.com>
2024-10-17 22:39:24 -0300

    feat: model for creating database

---
+++ CatalogAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CatalogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    DbSet<Catalog>? Catalogs { get; set; }
+    DbSet<Product>? Products { get; set; }
+
+}
