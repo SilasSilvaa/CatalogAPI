5a91336ec9b03bececd0dca36d4c4861ecd13cf4
SilasSilvaa <silas-oli@hotmail.com>
2024-10-17 22:39:45 -0300

    feat: model product

---
+++ CatalogAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogAPI.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? Image { get; set; }
+    public string? Inventory { get; set; }
+    public DateTime RegistrationData { get; set; }
+    public int CatalogId { get; set; }
+    public Catalog Catalog { get; set; }
+}
