e3a82f1cbef92e5b4e437c8f902d57d708194b50
SilasSilvaa <silas-oli@hotmail.com>
2024-10-18 00:07:44 -0300

    refactor: using data annotations

--- CatalogAPI/Models/Catalog.cs
+++ CatalogAPI/Models/Catalog.cs
@@ -1,13 +1,25 @@
-﻿namespace CatalogAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace CatalogAPI.Models;
+
+[Table("Catalogs")]
 public class Catalog
 {
     public Catalog()
     {
         Products = new List<Product>();
     }
+
+    [Key]
     public int CatalogId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? Image { get; set; }
     public ICollection<Product> Products { get; set; }
 }
